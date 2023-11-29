eef1c83fd5100437dafbaca9324fdf8899c313e8
Samarth Subramani Nagarathna <102940581+Samarth-Subramani@users.noreply.github.com>
2023-11-29 10:41:38 +0100

    Update exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -13,7 +13,7 @@ def fetch_data(url):
     return pd.read_csv(url, delimiter=';', on_bad_lines='skip')
 
 def write_to_sql(data_frame, database_url, table_name):
-  # Write Pandas DataFrame to SQLite database.
+    # Write Pandas DataFrame to SQLite database.
     engine = create_engine(database_url)
     data_frame.to_sql(table_name, engine, index=False, if_exists="replace", dtype={
         'column_1': Integer(),
@@ -31,14 +31,10 @@ def write_to_sql(data_frame, database_url, table_name):
         'geo_punkt': String()   
     })
 
-def main():
-    # Fetch data from the source
-    airports_data = fetch_data(data_source_url)
+# Fetch data from the source
+airports_data = fetch_data(data_source_url)
 
-    # Write data to SQLite database
-    write_to_sql(airports_data, database_url, table_name)
+# Write data to SQLite database
+write_to_sql(airports_data, database_url, table_name)
 
-    print("Data pipeline completed successfully.")
-
-if _name_ == "_main_":
-    main()
+print("Data pipeline completed successfully.")
