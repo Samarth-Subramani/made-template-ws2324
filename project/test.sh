963f503c79b721e769ae7ed330f06551b79646da
Samarth Subramani Nagarathna <102940581+Samarth-Subramani@users.noreply.github.com>
2023-12-06 09:55:44 +0100

    Create test.sh

---
+++ project/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Python packages
+pip install io
+pip install requests 
+pip install zipfile
+pip install pandas
+pip install pysqlite3
+
+python3 projectpipeline.py
+python3 test.py
