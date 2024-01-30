cddaf887ee97297e9a9c0092a24db95a202b7c7f
Samarth Subramani Nagarathna <102940581+Samarth-Subramani@users.noreply.github.com>
2024-01-30 23:58:43 +0100

    Update exercise5.py

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -15,7 +15,7 @@ with zipfile.ZipFile(gtfs_zip_file, 'r') as zip_ref:
 stops_file = os.path.join("GTFS_data", "stops.txt")
 
 # Step 3: Data Processing
-stops_df = pd.read_csv(stops_file)
+stops_df = pd.read_csv(stops_file, encoding="utf-8")
 
 # Check Shape and Types
 print("Shape:", stops_df.shape)  # Check the number of rows and columns
@@ -32,13 +32,18 @@ filtered_stops_df = stops_df[
 print("Quality: No. of Empty Values")
 print(filtered_stops_df.isnull().sum())  # Check for empty values
 
+# Convert columns to appropriate data types
+filtered_stops_df["stop_id"] = filtered_stops_df["stop_id"].astype(int)
+filtered_stops_df["zone_id"] = filtered_stops_df["zone_id"].astype(int)
+
 # Step 4: Write to SQLite Database
 conn = sqlite3.connect('gtfs.sqlite')
 filtered_stops_df.to_sql('stops', conn, if_exists='replace', index=False, dtype={
-    'stop_id': 'BIGINT',
+    'stop_id': 'INTEGER',
     'stop_name': 'TEXT',
     'stop_lat': 'FLOAT',
     'stop_lon': 'FLOAT',
-    'zone_id': 'BIGINT'
+    'zone_id': 'INTEGER'
 })
 conn.close()
+
